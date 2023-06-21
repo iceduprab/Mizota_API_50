4c58ef98f5acdc3863bddd1ee424f4cad15e3024
iceduprab <132135460+iceduprab@users.noreply.github.com>
2023-06-21 18:55:39 +0900

    Add files via upload

---
+++ YoutubeAPI/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    var API_KEY = "AIzaSyB_50fco3P30v4xlktJcDz_aFFELDQoDB4"
+
+    var video = '';
+
+    $("#form").submit(function(event){
+        event.preventDefault()
+        alert("Form is submitted")
+
+        var search = $("#search").val()
+
+        videoSearch(API_KEY,search,10)
+    })
+
+    function videoSearch(key, search, maxResults) {
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search, function (data) {
+            console.log(data)
+
+            data.items.forEach(item => {
+                video = `
+
+                <iframe width="420" height="315" src="http://www.youtube.com/embed/${item.id.videoID}" frameborder="0" allowfullscreen></iframe>
+
+                `
+                
+                $("#videos").append(video)
+                console.log(video)
+                $("#videos")
+            })
+        })
+
+    }
+})
